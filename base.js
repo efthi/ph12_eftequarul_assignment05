f7b6e0085c2942ed9158d53038d94ba4bb9b3aa3
Md Eftequarul Alam <efthaqur@gmail.com>
2025-08-29 03:01:37 +0600

    call functionality, call history clear function add

--- base.js
+++ base.js
@@ -3,6 +3,8 @@ JavaScript
 Author: Efthaqur Alam */
 
 
+
+
 // Heart count 
 let heartAmount = document.getElementById("heart_number");
 
@@ -17,3 +19,49 @@ for(let count of heartIco){
        
     });
 }
+
+// Copy count 
+
+
+
+
+// Call Functionality
+
+let dialCalls = document.getElementsByClassName("call_btn");
+
+for( let dialCall of dialCalls){
+            dialCall.addEventListener('click', function(){
+                let serviceNumber = dialCall.parentElement.previousElementSibling.previousElementSibling;
+                let serviceName = serviceNumber.previousElementSibling;
+                console.log(serviceName.innerText, serviceNumber.innerText);
+               let coinAmount = parseInt(document.getElementById("coin_number").innerText);
+                if(coinAmount >= 20 ){
+                    alert("Calling to: "+serviceName.innerText+", Service Number: "+serviceNumber.innerText);
+                    coinAmount = coinAmount - 20;
+                    document.getElementById("coin_number").innerText = coinAmount;
+                    
+                     let callLog = new Date().toLocaleDateString();
+                     let li = document.createElement("li")
+                     li.classList.add("list-row");
+                     li.innerHTML = `
+                     <p>${serviceName.innerText} - ${serviceNumber.innerText} - T: ${callLog}</p>
+                     `
+                     document.getElementById("call_logs").prepend(li);
+
+
+                }
+                else{
+                    alert("You don't have sufficient balance!");
+                }
+            })
+
+        }
+    
+    let callLogs = document.getElementById('call_logs');
+    let clearLogs = document.getElementById('clear_logs');
+    clearLogs.addEventListener('click', function(){
+        callLogs.innerHTML ="History Cleared";
+    })
+
+
+
